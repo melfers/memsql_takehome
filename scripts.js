23e03ec5df511ce9dafb097813325b912039ad82
Molly Elfers <elfers.molly@gmail.com>
2019-09-12 18:29:19 -0700

    GH commit

--- scripts.js
+++ scripts.js
@@ -2,15 +2,13 @@ const form = document.getElementById("form");
 const businessSize = document.getElementById("business_size");
 const headerValue = document.querySelector(".header_text");
 const subHeaderValue = document.querySelector(".subheader_text");
+const calendarIcon = document.querySelector(".far");
 
 form.addEventListener("submit", handleSubmit);
 
+function formValidator() {}
+
 function handleSubmit(event) {
-  console.log("handle submit was called");
-  console.log(event);
-
-  console.log(form.solution.value);
-
   event.preventDefault();
   if (
     businessSize == "1-10" ||
@@ -22,10 +20,14 @@ function handleSubmit(event) {
     subHeaderValue.innerHTML =
       "We'll let you know when the next one is available";
     form.classList.add("hidden");
+    calendarIcon.classList.remove("fa-calendar-alt");
+    calendarIcon.classList.add("fa-calendar-times");
   } else {
     headerValue.innerHTML = "Awesome! We'll contact you shortly";
     subHeaderValue.innerHTML =
       "We'll send you an email with available times to meet";
     form.classList.add("hidden");
+    calendarIcon.classList.remove("fa-calendar-alt");
+    calendarIcon.classList.add("fa-calendar-check");
   }
 }
